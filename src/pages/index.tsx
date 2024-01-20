d435aaa9d7f292d124ebad365a61c6a2295e8f9f
Pedro <peuhenry2018@gmail.com>
2024-01-20 14:16:47 -0300

    Template Initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styles from 'styles/home.module.scss'
+
+const HomePage = () => (
+  <div className={styles.container}>
+    <header>
+      <div className={styles.header__content}> header</div>
+      </header>
+    <main>main</main>
+  </div>
+)
+
+export default HomePage
